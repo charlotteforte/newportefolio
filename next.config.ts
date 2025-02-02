865ff838935590161757eaba4b26d8e65e695128
charlotteforte <charlotteforte808@gmail.com>
2025-02-02 11:49:54 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  // Optionnel : configuration pour ajouter d'autres modules ou plugins
+};
+
+export default nextConfig;
